495c9f3656ca7dfab13f574f927097b57e5c6f52
Selçuk Karateke <selcuk.karateke@live.de>
2019-10-08 21:48:33 +0200

    Erster Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Go!\n";
+$y = 1565200;
+$z = "";
+
+for ($i = 1; $i <= $y; $i++) {
+	echo "I: $i/$y D: " . intval($i / $y * 100) . "% \t \n";
+	
+    // sleep(0.1);
+    echo "\033[1A";
+	echo "";
+}
+echo "\033[1A";
+echo "Done!\t\n";
