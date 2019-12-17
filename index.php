90ea3566f81c7b367050ad75def498bbaeb9e88f
Selcuk Karateke <selcuk.karateke@live.de>
2019-12-17 13:25:33 +0100

    updated

--- index.php
+++ index.php
@@ -8,7 +8,9 @@ for ($i = 1; $i <= $y; $i++) {
 	echo "I: $i/$y D: " . intval($i / $y * 100) . "% \t \n";
 	
     // sleep(0.1);
+	echo "\033[?25l"; // hide cursor
     echo "\033[1A";
+	
 	echo "";
 }
 echo "\033[1A";
